0cd07df11dbbff277c81954ad25610d93e84859f
roomaisa2468 <varishauddin763@gmail.com>
2024-05-22 17:17:56 +0500

    Add files via upload

---
+++ js-assignment-1/index.js
@@ -0,0 +1,82 @@
+var greeting = "Hello! Welcome to our website.";
+
+alert(greeting);
+
+{
+    alert("Error: Please enter a valid password");
+};
+
+alert("Welcome to JS land\n\nHappy coding");
+
+
+
+// let weight = +prompt("enter weight");
+// let time = +prompt("enter time");
+
+// if (!(weight > 300 && time < 6)) {
+//     alert("come to our tryout")
+// } else {
+//     alert("Come to our cookout");
+// }
+
+alert("Welcome to JS land");
+alert("Happy coding\n\Prevent this page from creating additional dialogues");
+alert("Hello! I can run JS through my web browser console");
+
+
+///////////////chapter-2/////////////////////////
+/////////// ques-1 /////
+let username;
+console.log(username);
+////////////////// ques -2 /////////////////////
+var myName = "Your Full Name";
+var myName = "John Doe";
+/////////////////// ques-3 ///////
+var message;
+message = "Hello World";
+alert(message);
+
+/////////////////// ques-4 ///////////////////
+
+// Save student's bio data in JavaScript variables
+let studentName = "John Doe";
+let studentAge = 20;
+let studentCourse = "Web And Mobile App Developement";
+
+// Display the student's name in an alert box
+alert("Student Name: " + studentName);
+
+// Display the student's age in an alert box
+alert("Student Age: " + studentAge);
+
+// Display the student's course in an alert box
+alert("Student Course: " + studentCourse);
+//////////////////// ques-5 ///////////////////
+let pizza = "PIZZA";
+let result = "";
+
+for (let i = pizza.length; i > 0; i--) {
+    result += pizza.slice(0, i) + "\n";
+}
+
+alert(result);
+
+ //////////////////// ques -6 ///////////////
+
+ // Declare a variable called email and assign a string representing your email address
+let email = "abc@example.com";
+
+// Display a message containing the email address in an alert box using string concatenation
+alert(email);
+
+//////////////////// ques-7 //////////////////
+
+// Declare a variable called book and assign it the value "A smarter way to learn JavaScript"
+let book = "A smarter way to learn JavaScript";
+
+// Display a message in an alert box using the value of the book variable
+alert("I'm trying to learn JavaScript from the book '" + book + "'");
+
+ ///////////////////// ques-8 //////////////////
+
+ document.write("Yah! I can run my code through JavaScript");
